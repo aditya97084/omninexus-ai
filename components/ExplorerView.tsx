b47576cea46d8953dbed2f00f9197496ce2338c7
Aditya <adityaverma9708@gmail.com>
2026-02-16 18:22:41 +0530

    Deploy: components/ExplorerView.tsx

---
+++ components/ExplorerView.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+const ExplorerView = () => <div className="p-10 text-center"><h2 className="text-3xl font-black">Market Assets</h2><p className="text-slate-500">Synchronizing with global nodes...</p></div>;
+export default ExplorerView;
